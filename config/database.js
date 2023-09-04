c3c85ce50e7727a8efbcf3beb1010a62c8f2e082
apichet06 <apichet06@gmail.com>
2023-09-04 21:31:14 +0700

    2rd commit

--- config/database.js
+++ config/database.js
@@ -1,27 +1,29 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+
+const pool = mysql.createPool(process.env.DATABASE_URL)
+
 
 // Test the database connection
 pool.getConnection((err, connection) => {
-    if (err) {
-      console.error('Error connecting to the database:', err);
-      return;
-    }
-  
-    console.log('Connected to the database');
-  
-    connection.release();
-  });
-  
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+
+  console.log('Connected to the database');
+
+  connection.release();
+});
 
 module.exports = pool.promise();
